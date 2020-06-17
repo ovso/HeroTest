656c3b00f25abc7e62f42279d210eab6223debad
jaeho <ovsoce@gmail.com>
2020-06-17 17:17:36 +0900

    Apply databinding on A,B fragment

--- app/src/main/java/io/github/ovso/herotest/view/ui/screena/AFragment.kt
+++ app/src/main/java/io/github/ovso/herotest/view/ui/screena/AFragment.kt
@@ -1,36 +1,21 @@
 package io.github.ovso.herotest.view.ui.screena
 
-import android.content.Context
-import androidx.lifecycle.ViewModelProviders
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import io.github.ovso.herotest.R
+import io.github.ovso.herotest.databinding.FragmentABinding
+import io.github.ovso.herotest.exts.getViewModelFactory
+import io.github.ovso.herotest.view.base.DataBindingFragment
 import io.github.ovso.herotest.view.base.OnTextChangedListener
 import timber.log.Timber
 
-class AFragment : Fragment(), OnTextChangedListener {
+class AFragment : DataBindingFragment<FragmentABinding>(R.layout.fragment_a),
+  OnTextChangedListener {
 
   companion object {
     fun newInstance() = AFragment()
   }
 
-  private lateinit var viewModel: AViewModel
-
-  override fun onCreateView(
-    inflater: LayoutInflater, container: ViewGroup?,
-    savedInstanceState: Bundle?
-  ): View? {
-    return inflater.inflate(R.layout.fragment_a, container, false)
-  }
-
-  override fun onActivityCreated(savedInstanceState: Bundle?) {
-    super.onActivityCreated(savedInstanceState)
-    viewModel = ViewModelProviders.of(this).get(AViewModel::class.java)
-    // TODO: Use the ViewModel
-  }
+  private val viewModel: AViewModel by viewModels { getViewModelFactory() }
 
   override fun onTextChanged(text: String) {
     Timber.i("A onTextChanged($text)")
--- app/src/main/java/io/github/ovso/herotest/view/ui/screenb/BFragment.kt
+++ app/src/main/java/io/github/ovso/herotest/view/ui/screenb/BFragment.kt
@@ -1,35 +1,22 @@
 package io.github.ovso.herotest.view.ui.screenb
 
-import androidx.lifecycle.ViewModelProviders
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import io.github.ovso.herotest.R
+import io.github.ovso.herotest.databinding.FragmentBBinding
+import io.github.ovso.herotest.exts.getViewModelFactory
+import io.github.ovso.herotest.view.base.DataBindingFragment
 import io.github.ovso.herotest.view.base.OnTextChangedListener
 import timber.log.Timber
 
-class BFragment : Fragment(), OnTextChangedListener {
+class BFragment : DataBindingFragment<FragmentBBinding>(R.layout.fragment_b),
+  OnTextChangedListener {
 
   companion object {
     fun newInstance() = BFragment()
   }
 
-  private lateinit var viewModel: BViewModel
+  private val viewModel: BViewModel by viewModels { getViewModelFactory() }
 
-  override fun onCreateView(
-    inflater: LayoutInflater, container: ViewGroup?,
-    savedInstanceState: Bundle?
-  ): View? {
-    return inflater.inflate(R.layout.fragment_b, container, false)
-  }
-
-  override fun onActivityCreated(savedInstanceState: Bundle?) {
-    super.onActivityCreated(savedInstanceState)
-    viewModel = ViewModelProviders.of(this).get(BViewModel::class.java)
-    // TODO: Use the ViewModel
-  }
 
   override fun onTextChanged(text: String) {
     Timber.i("B onTextChanged($text)")
